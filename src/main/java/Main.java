7f43a4f73450f9a32eba6f7243bcfb408ea63c14
Frontol476 <danabek55@gmail.com>
2024-03-10 13:53:51 +0500

    First commit basic functions of Calculator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,46 @@
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Вас приветствует калькулятор! ");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество людей для которых требуется поделить счет");
+
+        boolean isCorrect = false;
+
+        //проверим на корректность ввода
+        int countOfPeople = 0;
+        while (!isCorrect) {
+            try {
+                countOfPeople = scanner.nextInt();
+            }catch (Exception e){
+                System.out.println("Допустим ввод только чисел");
+                continue;
+            }
+
+            isCorrect = checkErrors(countOfPeople);
+        }
+        System.out.println("Данные приняты количество людей: " + countOfPeople);
+        //Количество людей задали продолжаем, создаем калькулятор
+
+        Calculator calculator = new Calculator();
+        calculator.startAddItems();
+
     }
+
+
+
+    //Метод для проверки корректности ввода количества людей
+    private static  boolean checkErrors(int inputText) {
+
+        if (inputText <= 1){
+            System.out.println("Количество людей должно быть больше 1");
+            return false;
+        }else {
+            return true;
+        }
+
+    }
+
 }
